5676da69aeebfd0a1d6997772604fd4793e0597d
SAMSONSHUKURU <shukuru1@tritons.iowacentral.edu>
2022-09-13 10:29:37 -0500

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,31 @@
+/*
+Samson Shukuru
+C++ Fall 2022
+Due: Sep. 14, 202
+Lab 2
+Designed to get familiar output the command prompt using C++
+
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+
+{
+	// Asterick Strings A & B
+	string A = " * * * *";
+	string B = "* * * *";
+
+	// 8 string outputs
+	cout << A << endl;
+	cout << B << endl;
+	cout << A << endl;
+	cout << B << endl;
+	cout << A << endl;
+	cout << B << endl;
+	cout << A << endl;
+	cout << B << endl;
+	
+	return 0;
+}
